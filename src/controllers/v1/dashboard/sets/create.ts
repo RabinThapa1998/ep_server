4f74fc293362b03c7773ad6cfa448c957ebe202a
RabinThapa1998 <imrabin1998@gmail.com>
2022-11-29 08:49:40 +0545

    ✨ :Feat: Set Create Controller

--- src/controllers/v1/dashboard/sets/create.ts
+++ src/controllers/v1/dashboard/sets/create.ts
@@ -1,12 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError } from "../../../../common/errors/bad-request-error";
+import { Sets } from "../../../../models/sets.model";
 
 const createSets = async (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
   try {
-    if (!name) {
-      throw new BadRequestError("set name is required");
+    const _sets = await Sets.findOne({ name: name });
+    if (_sets) {
+      throw new BadRequestError("name already exists");
     }
+    const newSets = await Sets.build({
+      name,
+    }).save();
+
+    res.status(200).json({
+      data: newSets,
+    });
   } catch (error) {
     throw new BadRequestError(
       (error as any).message
